8b4f6cde518f0b17d665e5a06ef42f95f1a6aa07
shankar8 <shankar.krishna1989@gmail.com>
2017-02-14 11:11:09 -0500

    removed page navigation scroll coding

--- scripts/main.js
+++ scripts/main.js
@@ -4,39 +4,6 @@ $(function(){
 
 
 
-//init scrollmagic controller
-var controller = new ScrollMagic.Controller();
-
-//section pinning for each section
-
-$('.sectionBox').each(function(){
-
-      //init page navigation scene
-      var navScene = new ScrollMagic.Scene({
-            triggerElement: this,
-            triggerHook: 0
-      })
-      .setPin(this,{pushFollowers: false})
-      .addTo(controller)
-      .addIndicators({
-            name: 'section pin',
-            colorStart: 'orange'
-      });
-
-
-
-
-
-});
-
-
-
-
-
-
-
-
-
 
 
 
