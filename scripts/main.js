40e5ee33b78e853446974aa3e291ab5db607ab89
shankar8 <shankar.krishna1989@gmail.com>
2017-02-14 05:39:00 -0500

    changed css

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,36 @@ $(function(){
 
 
 
+//init scrollmagic controller
+var controller = new ScrollMagic.Controller();
+
+//section pinning for each section
+
+$('.sectionBox').each(function({
+
+      //init page navigation scene
+      var navScene = new ScrollMagic.Scene({
+            triggerElement: this,
+            triggerHook: 0
+      })
+      .setPin(this.imgBox,{pushFollowers: false})
+      .addTo(controller);
+
+
+
+
+
+}));
+
+
+
+
+
+
+
+
+
+
 
 
 });
