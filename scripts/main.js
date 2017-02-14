24167768ba61aa0e5f8435056061cb681fbe3960
shankar8 <shankar.krishna1989@gmail.com>
2017-02-14 03:20:35 -0500

    added scrollmagic page navigation and changed background image position in css

--- scripts/main.js
+++ scripts/main.js
@@ -5,84 +5,41 @@ $(function(){
 
       //init ScrollMagic
       var controller = new ScrollMagic.Controller();
-/*
-      //build a scene
-      var ourScene = new ScrollMagic.Scene({
-            triggerElement: '#landing h3',
-            // duration: '50%',  //duration of trigger - 100% of viewport
-            triggerHook: 0.9,  //ranges from 0 to 1
-            reverse: false   //animation will only happens once..by default its true
-      })
-      .setClassToggle('#landing','change')//add class to #landing
-      .addIndicators({
-            name: 'landing scene',
-            colorTrigger: 'red',
-            // indent: 200,  //indents indicator
-            colorStart: '#75C659',
-            colorEnd: 'purple'
-      })//add indicator plugin
-      .addTo(controller);//linked to controller
-*/
 
-      var pinLandingScene = new ScrollMagic.Scene({
-            triggerElement: '#landing',
-            triggerHook: 0,
-            // duration: '200%'
+      //Build Scene
+
+      var landingScene = new ScrollMagic.Scene({
+            triggerElement: '.trigger1',
+            duration: '50%',
+            triggerHook: 0.9,
+            reverse: true
       })
+      .setClassToggle('.landing','fade-in')
       .addIndicators({
-            name: 'landing scene',
-            colorTrigger: 'red',
-            // indent: 200,  //indents indicator
-            colorStart: '#75C659',
+            name: 'fade Scene',
+            colorTrigger: 'pink',
+            indent: 200,
+            colorStart: 'orange',
             colorEnd: 'purple'
-      })//add indicator plugin
-      .setPin('.landingImg')
+      })
       .addTo(controller);
 
 
-      function fix_scroll() {
-        var s = $(window).scrollTop();
-        var fixedTitle = $('.landingImg');
-        fixedTitle.css('position','absolute');
-        fixedTitle.css('top',s + 'px');
-      }fix_scroll();
 
-      $(window).on('scroll',fix_scroll);
+      //pin each Section
 
-
-
-
-
-
-
-
-
-      // loop through each element
-      // $('.sectionBox').each(function(){
-      //
-      //       console.log(this);
-      //
-      //       var ourScene = new ScrollMagic.Scene({
-      //             triggerElement: this.children[0],
-      //             triggerHook : 0.9
-      //       })
-      //       .setClassToggle(this, 'change')
-      //       .addIndicators({
-      //             name: 'fade scene',
-      //             colorTrigger: 'black',
-      //             colorStart: 'green',
-      //             colorEnd: 'purple'
-      //       })
-      //       .addTo(controller);
-      // });
-
-
-
-
-
-
-
-
+      $(".sectionBox").each(function(){
+            var sectionPin = new ScrollMagic.Scene({
+                  triggerElement: this,
+                  triggerHook: 0
+            })
+            .addIndicators({
+                  name: 'section pin'
+            })
+            .setPin(this)
+            .addTo(controller)
+            ;
+      })
 
 
 
