3cd0db4d36006c2b733eea18edcf055e136be798
shankar8 <shankar.krishna1989@gmail.com>
2017-02-16 21:52:55 -0500

    added map section and made changes to basic navigation

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,8 @@ var controller = new ScrollMagic.Controller();
 
 //landing2 timelineMax
 var landing2Tl = new TimelineMax();
-landing2Tl.to( '.landing2',0.6,{y:'-100%',ease: Power3.easeOut},0.4);
+landing2Tl.to( '.landing2',0.6,{y:'-100%',ease: Power3.easeOut})
+.to( '.landing h3',0.6,{y:'-200',opacity: 0,ease: Power3.easeOut},0);
 
 
 //init landing2 Scene
@@ -18,10 +19,10 @@ var landing2Scene = new ScrollMagic.Scene({
       trggerHook: 1
 })
 .setTween(landing2Tl)
-.addIndicators({
-      name: 'Section Trigger',
-      colorStart : 'pink'
-})
+// .addIndicators({
+//       name: 'Section Trigger',
+//       colorStart : 'pink'
+// })
 .addTo(controller);
 
 
@@ -29,7 +30,9 @@ var landing2Scene = new ScrollMagic.Scene({
 //////////////////////////  Destination scene
 //destination timelineMax
 var destinationTl = new TimelineMax();
-destinationTl.to( '.destination',0.6,{y:'-100%',ease: Power3.easeOut},0.4);
+destinationTl.to( '.destination',0.6,{y:'-100%',ease: Power3.easeOut},0.4)
+.to( '.landing2 h3',0.6,{y:'-200',opacity: 0,ease: Power3.easeOut},0);
+
 
 
 //init landing2 Scene
@@ -38,10 +41,35 @@ var destinationScene = new ScrollMagic.Scene({
       trggerHook: 1
 })
 .setTween(destinationTl)
-.addIndicators({
-      name: 'Section Trigger',
-      colorStart : 'pink'
+// .addIndicators({
+//       name: 'Section Trigger',
+//       colorStart : 'pink'
+// })
+.addTo(controller);
+
+
+
+
+
+
+//////////////////////////  Map scene
+//map timelineMax
+var mapTl = new TimelineMax();
+mapTl.to( '.map',0.6,{left:0,ease: Power3.easeOut},0.4)
+.to( '.destination h3',0.6,{x:'0',opacity: 0,ease: Power3.easeOut},0);
+
+
+
+//init landing2 Scene
+var mapScene = new ScrollMagic.Scene({
+      triggerElement: '.trigger4',
+      trggerHook: 1
 })
+.setTween(mapTl)
+// .addIndicators({
+//       name: 'Section Trigger',
+//       colorStart : 'pink'
+// })
 .addTo(controller);
 
 
@@ -51,19 +79,20 @@ var destinationScene = new ScrollMagic.Scene({
 //////////////////////////  moment scene
 //moment timelineMax
 var momentTl = new TimelineMax();
-momentTl.to( '.moment',0.6,{y:'-100%',ease: Power3.easeOut},0.4);
+momentTl.to( '.moment',0.6,{y:'-100%',ease: Power3.easeOut},0.4)
+.to( '.map h3',0.6,{y:'-200',opacity: 0,ease: Power3.easeOut},0);
 
 
 //init landing2 Scene
 var momentScene = new ScrollMagic.Scene({
-      triggerElement: '.trigger4',
+      triggerElement: '.trigger5',
       trggerHook: 1
 })
 .setTween(momentTl)
-.addIndicators({
-      name: 'Section Trigger',
-      colorStart : 'pink'
-})
+// .addIndicators({
+//       name: 'Section Trigger',
+//       colorStart : 'pink'
+// })
 .addTo(controller);
 
 
@@ -73,19 +102,45 @@ var momentScene = new ScrollMagic.Scene({
 //////////////////////////  memory scene
 //memory timelineMax
 var memoryTl = new TimelineMax();
-memoryTl.to( '.memory',0.6,{y:'-100%',ease: Power3.easeOut},0.4);
+memoryTl.to( '.memory',0.6,{y:'-100%',ease: Power3.easeOut},0.4)
+.to( '.moment h3',0.6,{y:'-200',opacity: 0,ease: Power3.easeOut},0);
 
 
 //init landing2 Scene
 var memoryScene = new ScrollMagic.Scene({
-      triggerElement: '.trigger5',
+      triggerElement: '.trigger6',
       trggerHook: 1
 })
 .setTween(memoryTl)
-.addIndicators({
-      name: 'Section Trigger',
-      colorStart : 'pink'
+// .addIndicators({
+//       name: 'Section Trigger',
+//       colorStart : 'pink'
+// })
+.addTo(controller);
+
+
+
+
+
+
+
+//////////////////////////  footer scene
+//footer timelineMax
+var footerTl = new TimelineMax();
+footerTl.to( '.memory',0.6,{y:'-150%',ease: Power3.easeOut})
+.to( '.footer',0.6,{y:'-50%',ease: Power3.easeOut},0);
+
+
+//init landing2 Scene
+var footerScene = new ScrollMagic.Scene({
+      triggerElement: '.trigger7',
+      trggerHook: 1
 })
+.setTween(footerTl)
+// .addIndicators({
+//       name: 'Section Trigger',
+//       colorStart : 'pink'
+// })
 .addTo(controller);
 
 
