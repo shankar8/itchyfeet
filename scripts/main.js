be6c585cb3f3da4a36ce92289ca8e0ac690167cc
shankar8 <shankar.krishna1989@gmail.com>
2017-02-22 21:59:06 -0500

    added svg frame over the landing page, basic map animation

--- scripts/main.js
+++ scripts/main.js
@@ -24,10 +24,76 @@ viewFrame.attr({
 
 $('.viewFrameBox').append($(".viewFrameSvg"));
 
+console.log(viewFrame.node);
 
 
 
 
+
+
+
+//using snap svg to animate route on the map
+var route = Snap('#route');
+
+route.attr({strokeDasharray: 740,
+strokeDashoffset: 740});
+
+function routeAnimate(){
+      route.animate({strokeDashoffset: 0}, 600);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//stop scrolling while animating
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+      e.preventDefault();
+  e.returnValue = false;
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+function disableScroll() {
+  if (window.addEventListener) // older FF
+      window.addEventListener('DOMMouseScroll', preventDefault, false);
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove  = preventDefault; // mobile
+  document.onkeydown  = preventDefaultForScrollKeys;
+}
+
+function enableScroll() {
+    if (window.removeEventListener)
+        window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.onmousewheel = document.onmousewheel = null;
+    window.onwheel = null;
+    window.ontouchmove = null;
+    document.onkeydown = null;
+}
+
+
+/*------------------------------------------------------------
+------------------------------------------------------------*/
 
 
 
@@ -42,9 +108,10 @@ var controller = new ScrollMagic.Controller();
 
 //landing2 timelineMax
 var landing2Tl = new TimelineMax();
-landing2Tl.to( '.landing2',0.6,{y:'-100%',ease: Power3.easeOut})
-.to( '.landing h3',0.6,{y:'-200',opacity: 0,ease: Power3.easeOut},0)
-.to('.viewFrameBox',0.5,{
+landing2Tl.to( '.landing2',0.8,{y:'-100%',ease: Power3.easeOut})
+.to( '.landing h3',0.8,{y:'-200',opacity: 0,
+      onStart:disableScroll, onComplete:enableScroll, ease: Power3.easeOut},0)
+.to('.viewFrameBox',0.6,{
       top: "50%",
       left: "50%",
       y: "-50%",
@@ -71,7 +138,8 @@ var landing2Scene = new ScrollMagic.Scene({
 //////////////////////////  Destination scene
 //destination timelineMax
 var destinationTl = new TimelineMax();
-destinationTl.to( '.destination',0.6,{y:'-100%',ease: Power3.easeOut},0.4)
+destinationTl.to( '.destination',0.6,{y:'-100%',
+      onStart:disableScroll, onComplete:enableScroll, ease: Power3.easeOut},0.4)
 .to( '.landing2 h3',0.6,{y:'-200',opacity: 0,ease: Power3.easeOut},0)
 .to('.viewFrameBox',0.6,{
       width:"25%",
@@ -101,8 +169,10 @@ var destinationScene = new ScrollMagic.Scene({
 //////////////////////////  Map scene
 //map timelineMax
 var mapTl = new TimelineMax();
-mapTl.to( '.map',0.6,{left:0,ease: Power3.easeOut},0.4)
-.to( '.destination h3',0.6,{x:'0',opacity: 0,ease: Power3.easeOut},0)
+mapTl.to( '.destination h3',0.6,{x:'0',opacity: 0,ease: Power3.easeOut},0)
+.to( '.map',0.6,{left:0,
+      ease: Power3.easeOut,
+      onStart: routeAnimate},0.4)
 .to('.viewFrameBox',0.6,{
       width: 0.2*wHeight,
       height: 0.2*wHeight
@@ -133,7 +203,13 @@ momentTl.to( '.moment',0.6,{y:'-100%',ease: Power3.easeOut},0.4)
 .to('.viewFrameBox',0.6,{
       width:0.7*wHeight,
       height: 0.7*wHeight
-},0.3);
+},0.3)
+.to(viewFrame.node,0.6,{
+      fill: 'none',
+      stroke: 'black',
+      strokeWidth: '20'
+},0.3)
+;
 
 
 //init landing2 Scene
@@ -160,7 +236,13 @@ memoryTl.to( '.memory',0.6,{y:'-100%',ease: Power3.easeOut},0.4)
 .to('.viewFrameBox',0.6,{
       width: wWidth,
       height: wHeight
-},0.3);
+},0.3)
+.to(viewFrame.node,0.6,{
+      fill: 'none',
+      stroke: 'white',
+      strokeWidth: '20'
+},0.3)
+;
 
 
 //init landing2 Scene
